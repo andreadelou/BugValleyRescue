9699198552bb1c9d43dbe1eef8b56aa6590dc584
Andrea Lam <lam20102@uvg.edu.gt>
2024-10-21 23:57:32 -0600

    movimiento del personaje

---
+++ BugValleyRescue/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    private CharacterController characterController;
+    /*private Animator animator;*/
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        /*animator = GetComponent<Animator>();*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movementVector = new Vector3(moveX, 0, moveZ);
+
+        if (movementVector.magnitude != 0)
+        {
+            transform.forward = movementVector;
+            characterController.SimpleMove(movementVector * speed);
+            /*animator.SetFloat("Movement", movementVector.magnitude);*/
+        }
+        else
+        {
+            /*animator.SetFloat("Movement", 0);*/
+        }
+    }
+}
